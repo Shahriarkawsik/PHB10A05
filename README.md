1f75b94bb22b098b66bff189a1baee60f3f1f19d
Shahriar <shahriarkawsik@gmail.com>
2024-09-22 19:53:49 +0600

    complete header section

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Live link ☞ 
+# Live link ☞ https://shahriarkawsik.github.io/PHB10A05
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {      
+      colors:{
+        'bgc-1' : "rgb(249, 247, 243)",
+        'btnColor' : "rgb(180, 244, 97)",
+        'btnTextColor' : "rgb(17,17, 17)",
+        'btnTextColor-2' : "rgba(17,17, 17,0.7)",
+      },
+      fontFamily:{
+        'lexendFont' : "Lexend",
+      },
+      fontSize:{
+        '20' : '20px',
+      }
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+}
